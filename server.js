d9ad21bc4d8c16f4e61721a9e32108bdb46d7713
JennaMetro <jennakop@metropolia.fi>
2019-05-08 13:08:10 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-var express = require('express');
-var path = require('path');
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 4200;
+const app = express();
 
-var port = process.env.PORT || 4200;
-var app = express();
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
 
-app.use(express.static('./dist/mlb-scores'));
-
-app.listen(port, function () {
-  console.log('Server is running.. on ' + Port + ' something');
+// send the user to index html page inspite of the url
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
 });
