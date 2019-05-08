320e8167d9d65c6ef80b46bb8864a201fed329ad
JennaMetro <jennakop@metropolia.fi>
2019-05-08 13:32:22 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ const port = process.env.PORT || 4000;
 const app = express();
 
 // the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/dist'));
 
 // send the user to index html page inspite of the url
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname+'/src', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+app.listen(process.env.PORT || port, function () {
+  console.log('Server is running.. on Port something');
 });
