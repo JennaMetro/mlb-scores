52903696792997356f37cc6cddd41c686e23cadc
JennaMetro <jennakop@metropolia.fi>
2019-05-08 12:01:22 +0300

    Start of project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpResponse} from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mlb-scores';
+  private apiUrl = '';
+  data: any = {};
+
+constructor(private http: HttpClient) {
+  console.log("hello works");
+  this.getGames();
+  this.getData();
 }
+getData(){
+  return this.http.get(this.apiUrl).pipe(map((response: any) => response.json()));
+}
+
+getGames(){
+  this.getData().subscribe(data => {
+    console.log(data);
+    this.data = data
+  })
+}
+}
+
+
+
